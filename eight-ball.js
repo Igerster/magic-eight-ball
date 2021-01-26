3eae6acae4dca44319623c2cf74bb5d3ab3b7c02
Nick <igerster80@gmail.com>
2021-01-26 18:04:35 -0500

    fixed issue with only running once.

--- eight-ball.js
+++ eight-ball.js
@@ -1,5 +1,3 @@
-let randomNumber = Math.floor(Math.random() * 8);
-let eightBall = "";
 function userName() {
   let userName = prompt("What is your name?")
   if (userName != null){
@@ -13,33 +11,33 @@ function userQuestion() {
   }
 }
 function answer(){
+  let randomNumber = Math.floor(Math.random() * 8);
+  let eightBall = "";
+  switch(randomNumber){
+    case 0:
+      eightBall = "It is certain";
+      break;
+     case 1:
+      eightBall = "It is decidedly so";
+      break;
+    case 2:
+      eightBall = "Reply hazy try again";
+      break;
+    case 3:
+      eightBall = "Cannot predict now";
+      break;
+    case 4:
+      eightBall = "Do not count on it";
+      break;
+    case 5:
+      eightBall = "My sources say no";
+      break;
+    case 6:
+      eightBall = "Outlook not so good";
+      break;
+    case 7:
+      eightBall = "Signs point to yes";
+      break;
+}
   let answer = document.getElementById("answer").innerHTML =  "The magic eight ball says... " + eightBall;
 }
-
-switch(randomNumber){
-  case 0:
-    eightBall = "It is certain";
-    break;
-   case 1:
-    eightBall = "It is decidedly so";
-    break;
-  case 2:
-    eightBall = "Reply hazy try again";
-    break;
-  case 3:
-    eightBall = "Cannot predict now";
-    break;
-  case 4:
-    eightBall = "Do not count on it";
-    break;
-  case 5:
-    eightBall = "My sources say no";
-    break;
-  case 6:
-    eightBall = "Outlook not so good";
-    break;
-  case 7:
-    eightBall = "Signs point to yes";
-    break;
-}
-
